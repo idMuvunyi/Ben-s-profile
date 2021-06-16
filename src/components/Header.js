eef5c611b994291dbdd7c71ec11c0cc6c9ae62b5
Muvunyi Idrissa <muvunyiiddy@gmail.com>
2021-06-16 12:05:11 +0200

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import profile from '../images/profile.jpg'
+
+export default function Header() {
+  return (
+    <>
+      <div className="navbar">
+        <div className="container flex">
+          <h2>Ben Ntwali</h2>
+          <nav>
+            <ul>
+              <li>
+                <a href="index.js">Home</a>
+                <a href="expertise.js">Expertise</a>
+                <a href="contact.js">Contact</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <section>
+        <div className="container grid">
+          <div className="text-intro">
+            <h1>
+              Hello, <br />
+              My name is Ben Ntwali. Just call me <b>Ben</b>!
+            </h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora
+              mollitia, voluptates optio dicta omnis nihil consequatur debitis
+              non. Mollitia veritatis aliquid ipsa excepturi dolorum sit nemo
+              optio vel necessitatibus delectus?
+            </p>
+          </div>
+
+          <img src={profile} alt="Ben profile" />
+        </div>
+      </section>
+    </>
+  )
+}
