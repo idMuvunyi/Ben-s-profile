8f0f5977e61cc1c261690ec16c0036695a4784a0
Muvunyi Idrissa <muvunyiiddy@gmail.com>
2021-06-17 11:21:45 +0100

    Add expertise section

--- src/components/Middle.js
+++ src/components/Middle.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 export default function Middle() {
     return (
+        <>
         <section className="middle">
             <div className="container">
              <div className="about-me">
@@ -27,5 +28,37 @@ export default function Middle() {
              </div>
             </div>
         </section>
+       <section className="expertise">
+
+           <h2>MY EXPERTISE</h2>
+           <div className="container flex">
+           <div className="card">
+              <h4>Embedded systems</h4>
+              <p>Tempora mollitia, voluptates optio dicta omnis 
+                  nihil consequatur debitis non. Mollitia veritatis 
+                  aliquid ipsa excepturi dolorum sit nemo optio vel 
+                  necessitatibus delectus</p>
+           </div>
+
+           <div className="card">
+              <h4>Electronic & PCB Design</h4>
+              <p>Tempora mollitia, voluptates optio dicta omnis 
+                  nihil consequatur debitis non. Mollitia veritatis 
+                  aliquid ipsa excepturi dolorum sit nemo optio vel 
+                  necessitatibus delectus</p>
+           </div>
+
+           <div className="card">
+              <h4>Android Development</h4>
+              <p>Tempora mollitia, voluptates optio dicta omnis 
+                  nihil consequatur debitis non. Mollitia veritatis 
+                  aliquid ipsa excepturi dolorum sit nemo optio vel 
+                  necessitatibus delectus</p>
+           </div>
+           </div>
+
+       </section>
+
+        </>
     )
 }
